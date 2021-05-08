c0a797a0f48608282b887fb956b4320c621b8718
lakshyamcs16 <lakshyamcs16>
2021-05-08 22:24:12 +0530

    583. Delete Operation for Two Strings

---
+++ Dynamic Programing/src/com/gfg/setone/583._Delete_Operation_for_Two_Strings.java
@@ -0,0 +1,36 @@
+/*
+
+Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.In one step, you can delete exactly one character in either string. Example 1:Example 2: Constraints:
+
+
+*/
+
+/*
+
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+Input: word1 = "leetcode", word2 = "etco"
+Output: 4
+
+
+
+*/
+
+public class Solution {
+    public int minDistance(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = i + j;
+                else if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+}
+​
