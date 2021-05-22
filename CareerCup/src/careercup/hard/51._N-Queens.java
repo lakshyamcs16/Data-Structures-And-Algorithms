8542f53d9ff53c4f2422c444547e2634edd716e8
lakshyamcs16 <32986427+lakshyamcs16@users.noreply.github.com>
2021-05-22 19:34:44 +0530

    Update 51._N-Queens.java

--- CareerCup/src/careercup/hard/51._N-Queens.java
+++ CareerCup/src/careercup/hard/51._N-Queens.java
@@ -18,60 +18,89 @@ Output: [["Q"]]
 */
 
 class Solution {
-    public List<List<String>> solveNQueens(int n) {        
-        int[][] board = new int[n][n];
-        List<List<String>> res = new ArrayList<List<String>>();
-​
-        nqueens(res, board, 0, 0, n, 1);
-        
-        
-        return res;
-    }
-    
-    private void nqueens(List<List<String>> res, int[][] board, int x, int y, int n, int index) {
-        if(x<n && x>=0 && y<n && y>=0) {
-            for(int i=0; i<n; i++) {
-                if(isValid(board, i, y, n)) {
-                    board[i][y] = 1;
-                    
-                    if(index == n) {
-                        addBoardToResult(res, board);
-                    }
-                    
-                    nqueens(res, board, i, y + 1, n, index + 1);
-                }
-​
-                board[i][y] = 0;
-            }
-        }        
-    }
-    
-    private boolean isValid(int[][] board, int x, int y, int n) {
-        if(x < n && y < n) {
-            //check row
-            for(int i=0; i<n; i++) {
-                if(y != i && board[x][i] == 1) {
-                    return false;
-                }
-            }
-            
-            //check diag
-            int i = x + 1, j = y + 1;
-            while(i<n && j<n) {
-                if(board[i][j] == 1) return false;
-                i++; j++;
-            }
-            
-            i = x - 1; j = y - 1;
-            while(i>=0 && j>=0) {
-                if(board[i][j] == 1) return false;
-                i--; j--;
-            }
-            
-            i = x + 1; j = y - 1;
-            while(i<n && j>=0) {
-                if(board[i][j] == 1) return false;
-                i++; j--;
-            }
-            
-​
+    public List<List<String>> solveNQueens(int n) {        
+        int[][] board = new int[n][n];
+        List<List<String>> res = new ArrayList<List<String>>();
+
+        nqueens(res, board, 0, 0, n, 1);
+        
+        
+        return res;
+    }
+    
+    private void nqueens(List<List<String>> res, int[][] board, int x, int y, int n, int index) {
+        if(x<n && x>=0 && y<n && y>=0) {
+            for(int i=0; i<n; i++) {
+                if(isValid(board, i, y, n)) {
+                    board[i][y] = 1;
+                    
+                    if(index == n) {
+                        addBoardToResult(res, board);
+                    }
+                    
+                    nqueens(res, board, i, y + 1, n, index + 1);
+                }
+
+                board[i][y] = 0;
+            }
+        }        
+    }
+    
+    private boolean isValid(int[][] board, int x, int y, int n) {
+        if(x < n && y < n) {
+            //check row
+            for(int i=0; i<n; i++) {
+                if(y != i && board[x][i] == 1) {
+                    return false;
+                }
+            }
+            
+            //check diag
+            int i = x + 1, j = y + 1;
+            while(i<n && j<n) {
+                if(board[i][j] == 1) return false;
+                i++; j++;
+            }
+            
+            i = x - 1; j = y - 1;
+            while(i>=0 && j>=0) {
+                if(board[i][j] == 1) return false;
+                i--; j--;
+            }
+            
+            i = x + 1; j = y - 1;
+            while(i<n && j>=0) {
+                if(board[i][j] == 1) return false;
+                i++; j--;
+            }
+            
+            i = x - 1; j = y + 1;
+            while(i>=0 && j<n) {
+                if(board[i][j] == 1) return false;
+                i--; j++;
+            }
+            
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private void addBoardToResult(List<List<String>> res, int[][] board) {
+        
+        ArrayList<String> cur = new ArrayList<String>();
+        for(int[] row:board) {
+            StringBuilder str = new StringBuilder();
+            for(int val:row) {
+                if(val == 1) {
+                    str.append("Q");
+                }else{
+                    str.append(".");
+                }
+            }
+            cur.add(str.toString());
+        }
+        
+        res.add(cur);
+    }
+}
