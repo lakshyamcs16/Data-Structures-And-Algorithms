d31f3a889000abd0a9c43783aab0b7c7a151a81e
MMT9387 <lakshya.sethi@go-mmt.com>
2022-05-31 05:47:45 +0530

    919: Meeting rooms 2

---
+++ Blind/Intervals/Meeting rooms II.js
@@ -0,0 +1,68 @@
+// https://www.lintcode.com/problem/919/
+
+/*
+ Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.)
+
+Contact me on wechat to get Amazon、Google requent Interview questions . (wechat id : jiuzhang15)
+
+
+(0,8),(8,10) is not conflict at 8
+
+Example
+Example1
+
+Input: intervals = [(0,30),(5,10),(15,20)]
+Output: 2
+Explanation:
+We need two meeting rooms
+room1: (0,30)
+room2: (5,10),(15,20)
+Example2
+
+Input: intervals = [(2,7)]
+Output: 1
+Explanation: 
+Only need one meeting room
+*/
+
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+export class Solution {
+    /**
+     * @param intervals: an array of meeting time intervals
+     * @return: the minimum number of conference rooms required
+     */
+
+    minMeetingRooms(intervals) {
+        // Write your code here
+        const starts = intervals.map(i => i.start).sort((x, y) => x - y);
+        const ends = intervals.map(i => i.end).sort((x, y) => x - y);
+
+        let i = 0, j = 0;
+        let rooms = 0, res = 0;
+
+        while (i < starts.length && j < ends.length) {
+            console.log(rooms, starts[i], ends[j]);
+
+            if (starts[i] < ends[j]) {
+                rooms++;
+                i++;
+            } else {
+                rooms--;
+                j++;
+            }
+
+            res = Math.max(res, rooms);
+        }
+
+        return res;
+    }
+}
